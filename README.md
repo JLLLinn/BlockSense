df5963e5f33f336919c46f50b3738a27ccc467f7
Jiaxin from Server <jiaxinlin01@gmail.com>
2018-07-11 20:28:50 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# BlockSense
+### Install Mongo to Store Tweets
+https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
+```
+$ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+$ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+$ sudo apt-get update
+$ sudo apt-get install -y mongodb-org
+$ sudo service mongod start
+$ cat /var/log/mongodb/mongod.log
+```
+### Install PyMongo as the python mongodb driver
+http://api.mongodb.com/python/current/
+``` 
+$ python3 -m pip install pymongo 
+```
+### Create a database named "twitterdb" for storing tweets
+
---
+++ tweepyGetTweets.py
@@ -0,0 +1,70 @@
+
+from __future__ import print_function
+from datetime import datetime
+from pytz import timezone
+import pytz
+import tweepy
+import json
+from pymongo import MongoClient
+
+date_format='%m/%d/%Y %H:%M:%S %Z'
+date = datetime.now(tz=pytz.timezone('US/Pacific'))
+print("****************************************************************")
+print("***** Get Tweet Program Starts at", date.strftime(date_format), "*****")
+print("****************************************************************")
+
+MONGO_HOST= 'mongodb://localhost/twitterdb'
+WORDS = ['BTC', 'Ethereum', 'blockchain', 'Bitcoin', 'ETH']
+
+twitterSecrets = {}
+with open('../twitterSecrets.json') as json_data:
+	twitterSecrets = json.load(json_data)
+	print("Twitter Secrets: ", twitterSecrets)
+
+CONSUMER_KEY = twitterSecrets['CONSUMER_KEY']
+CONSUMER_SECRET = twitterSecrets["CONSUMER_SECRET"]
+ACCESS_TOKEN = twitterSecrets["ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = twitterSecrets["ACCESS_TOKEN_SECRET"]
+
+class StreamListener(tweepy.StreamListener):    
+    #This is a class provided by tweepy to access the Twitter Streaming API. 
+
+    def on_connect(self):
+        # Called initially to connect to the Streaming API
+        print("You are now connected to the streaming API.")
+ 
+    def on_error(self, status_code):
+        # On error - if an error occurs, display the error / status code
+        print('An Error has occured: ' + repr(status_code))
+        return False
+ 
+    def on_data(self, data):
+        #This is the meat of the script...it connects to your mongoDB and stores the tweet
+        try:
+            client = MongoClient(MONGO_HOST)
+            
+            # Use twitterdb database. If it doesn't exist, it will be created.
+            db = client.twitterdb
+    
+            # Decode the JSON from Twitter
+            datajson = json.loads(data)
+            
+            #grab the 'created_at' data from the Tweet to use for display
+            created_at = datajson['created_at']
+
+            #print out a message to the screen that we have collected a tweet
+            print("Tweet collected at " + str(created_at))
+            
+            #insert the data into the mongoDB into a collection called twitter_search
+            #if twitter_search doesn't exist, it will be created.
+            db.twitter_search.insert(datajson)
+        except Exception as e:
+           print(e)
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+#Set up the listener. The 'wait_on_rate_limit=True' is needed to help with Twitter API rate limiting.
+listener = StreamListener(api=tweepy.API(wait_on_rate_limit=True)) 
+streamer = tweepy.Stream(auth=auth, listener=listener)
+print("Tracking: " + str(WORDS))
+streamer.filter(track=WORDS)
